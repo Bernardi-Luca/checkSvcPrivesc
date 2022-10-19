de9a9e0aa4ecf9af8371f18efa16a7ac5ee29fc2
Bernardi-Luca <79511920+Bernardi-Luca@users.noreply.github.com>
2022-10-19 10:27:38 +0200

    Update servicesCheck.ps1
    smarter access level output grabbing

--- servicesCheck.ps1
+++ servicesCheck.ps1
@@ -6,8 +6,9 @@ $userCheck=$env:username
 Get-WmiObject win32_service | ForEach-Object{
 	
 	#getting the type of privilege on the service for the current user
-	$SvcAccessLvl=.\accesschk64.exe /accepteula -uc $userCheck $_.name | findstr.exe $_.name
-	$SvcAccessLvlSplit=$SvcAccessLvl.Split(" ")[0]
+	if($SvcAccessLvl=.\accesschk64.exe /accepteula -uc $userCheck $_.name -nobanner){
+	    $SvcAccessLvlSplit=$SvcAccessLvl.Split(" ")[0]
+    }
 
 	#checking if the current user has write access to the service
 	if($SvcAccessLvlSplit -like '*W*')
@@ -16,41 +17,42 @@ Get-WmiObject win32_service | ForEach-Object{
         write-host "!!!!! MODIFIABLE SERVICE !!!!!"
 
         #service name output
-		write-host "--- SERVICENAME ---";
-		write-host $_.name;
-
-		#permissions on the service binary
-		#write-host "--- ASSOCIATED BINARY FILE PERMISSIONS ---";
-		#icacls $_.pathname.Split(" ")[0];
+		write-host "--- SERVICENAME ---"
+		write-host $_.name
 
 		#displaying permissions of current user on the service
-		write-host "--- CURRENT USER PERMISSION ON SERVICE ---";
-		.\accesschk64.exe /accepteula -ucv $userCheck $_.name;
+		write-host "--- CURRENT USER PERMISSION ON SERVICE ---"
+		.\accesschk64.exe /accepteula -ucv $userCheck $_.name
 
 		#displaying the user starting the service
-		write-host "--- DISPLAY ACCOUNT STARTING THE SERVICE ---";
-		sc.exe qc $_.name | findstr.exe "SERVICE_START_NAME";
+		write-host "--- DISPLAY ACCOUNT STARTING THE SERVICE ---"
+		sc.exe qc $_.name | findstr.exe "SERVICE_START_NAME"
 
 
 		echo "-------------------"
+        $SvcAccessLvlSplit=""
 	}
 
-	$SvcFileAccessLvl=.\accesschk64.exe /accepteula -uwqs $userCheck $_.pathname.Split(" ")[0]
-	$SvcFileAccessLvlSplit=$SvcFileAccessLvl.Split(" ")[0]
+	if ($SvcFileAccessLvl=.\accesschk64.exe /accepteula -uwqs $userCheck $_.pathname.Split(" ")[0] -nobanner){
+	    $SvcFileAccessLvlSplit=$SvcFileAccessLvl.Split(" ")[0]
+    }
 	
 	if($SvcFileAccessLvlSplit -like '*W*'){
 
         write-host "!!!!! MODIFIABLE SERVICE BINARY !!!!!"
 
 		#service name output
-		write-host "--- SERVICENAME ---";
-		write-host $_.name;
+		write-host "--- SERVICENAME ---"
+		write-host $_.name
 		
 		#permissions of the current user on the service binary
 		.\accesschk64.exe /accepteula -uwqs $userCheck $_.pathname.Split(" ")[0]
 		
 		#displaying the user starting the service
-		write-host "--- DISPLAY ACCOUNT STARTING THE SERVICE ---";
-		sc.exe qc $_.name | findstr.exe "SERVICE_START_NAME";
+		write-host "--- DISPLAY ACCOUNT STARTING THE SERVICE ---"
+		sc.exe qc $_.name | findstr.exe "SERVICE_START_NAME"
+
+        echo "-------------------"
+        $SvcFileAccessLvlSplit=""
 	}
 }
